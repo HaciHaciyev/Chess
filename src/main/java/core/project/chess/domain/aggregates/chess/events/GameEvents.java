cc9d1094abc5089526da5df6c804c88254f14d7a
Ain Grace <qnicat32@gmail.com>
2024-07-18 17:45:46 +0400

    add validation to GameEvents

--- src/main/java/core/project/chess/domain/aggregates/chess/events/GameEvents.java
+++ src/main/java/core/project/chess/domain/aggregates/chess/events/GameEvents.java
@@ -1,15 +1,24 @@
 package core.project.chess.domain.aggregates.chess.events;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Slf4j
 public record GameEvents(LocalDateTime creationDate,
                          LocalDateTime lastUpdateDate) {
 
     public GameEvents {
-        if (Objects.isNull(creationDate) || Objects.isNull(lastUpdateDate)) {
-            // TODO
-        }
+        creationDate = Objects.requireNonNullElseGet(creationDate, () -> {
+            log.error("creationDate is null, falling back to default value");
+            return LocalDateTime.now();
+        });
+
+        lastUpdateDate = Objects.requireNonNullElseGet(lastUpdateDate, () -> {
+            log.error("lastUpdateDate is null, falling back to default value");
+            return LocalDateTime.now();
+        });
     }
 
     public static GameEvents defaultEvents() {
