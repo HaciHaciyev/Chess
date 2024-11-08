59c20568b847eabf82d8b62736349b656e723410
HaciHaciyev <gadzievgadzi508@gmail.com>
2024-11-09 03:29:13 +0400

    Simple refactoring

--- src/main/java/core/project/chess/domain/aggregates/user/value_objects/Username.java
+++ src/main/java/core/project/chess/domain/aggregates/user/value_objects/Username.java
@@ -5,23 +5,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public record Username(String username) {
-    //TODO почему тут пробел в regEX?
+
     private static final String usernameRegex = "^[a-zA-Z0-9 ]*$";
 
     private static final Pattern pattern = Pattern.compile(usernameRegex);
 
-    //TODO нет ограничения на длину имени
     public Username {
         if (Objects.isNull(username)) {
-            throw new IllegalArgumentException("Username cannot be null");
+            throw new IllegalArgumentException("Username cannot be null.");
         }
         if (username.isBlank()) {
-            throw new IllegalArgumentException("Username cannot be blank");
+            throw new IllegalArgumentException("Username cannot be blank.");
+        }
+        if (username.length() > 24) {
+            throw new IllegalArgumentException("This username is to long.");
         }
 
         Matcher matcher = pattern.matcher(username);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException("Username contains invalid characters");
+            throw new IllegalArgumentException("Username contains invalid characters.");
         }
     }
 }
