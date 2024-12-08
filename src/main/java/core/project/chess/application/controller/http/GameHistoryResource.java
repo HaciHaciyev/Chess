cb9b419dd9ed9fb0181e39e03e33133b0a4914d8
HaciHaciyev <gadzievgadzi508@gmail.com>
2024-12-09 00:43:58 +0400

    deep refactore of application layer

---
+++ src/main/java/core/project/chess/application/controller/http/GameHistoryResource.java
@@ -0,0 +1,40 @@
+package core.project.chess.application.http;
+
+import core.project.chess.domain.aggregates.chess.value_objects.ChessGameHistory;
+import core.project.chess.domain.aggregates.user.value_objects.Username;
+import core.project.chess.domain.repositories.outbound.OutboundChessRepository;
+import io.quarkus.security.Authenticated;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import java.util.List;
+
+@Authenticated
+@Path("/account")
+public class GameHistoryResource {
+
+    private final JsonWebToken jwt;
+
+    private final OutboundChessRepository outboundChessRepository;
+
+    GameHistoryResource(JsonWebToken jwt, OutboundChessRepository outboundChessRepository) {
+        this.jwt = jwt;
+        this.outboundChessRepository = outboundChessRepository;
+    }
+
+    @GET
+    @Path("/game-history")
+    public Response gameHistory(@QueryParam("pageNumber") int pageNumber) {
+        List<ChessGameHistory> listOfGames = outboundChessRepository
+                .listOfGames(new Username(jwt.getName()), pageNumber)
+                .orElseThrow(
+                        () -> new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity("User does not exist.\uD83D\uDC7B").build())
+                );
+
+        return Response.ok(listOfGames).build();
+    }
+}
