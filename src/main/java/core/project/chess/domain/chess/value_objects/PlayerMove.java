39c0b40e67c0f860f10ef8deca18d290e2e8ac82
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-02-19 07:23:47 +0400

    Fix: remove unsafe implementation of 'equals' method
    Record have it`s own implementation of `equals`

--- src/main/java/core/project/chess/domain/chess/value_objects/PlayerMove.java
+++ src/main/java/core/project/chess/domain/chess/value_objects/PlayerMove.java
@@ -12,10 +12,4 @@ public record PlayerMove(Coordinate from, Coordinate to, @Nullable Piece promoti
         Objects.requireNonNull(from);
         Objects.requireNonNull(to);
     }
-
-    @Override
-    public final boolean equals(Object other) {
-        var move = (PlayerMove) other;
-        return move.from.equals(this.from) && move.to.equals(this.to);
-    }
 }
