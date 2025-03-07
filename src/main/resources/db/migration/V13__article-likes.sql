ba8352cf536335701b2e85d896c973baa3464f63
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-03-07 05:55:29 +0400

    Fix: sql migration related to article likes

--- src/main/resources/db/migration/V13__article-likes.sql
+++ src/main/resources/db/migration/V13__article-likes.sql
@@ -2,5 +2,7 @@ CREATE TABLE Like (
     article_id CHAR(36) NOT NULL,
     user_id CHAR(36) NOT NULL,
     creation_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (article_id, user_id)
+    PRIMARY KEY (article_id, user_id),
+    CONSTRAINT like_article_fk FOREIGN KEY (article_id) REFERENCES Article(id),
+    CONSTRAINT like_user_fk FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
 );
