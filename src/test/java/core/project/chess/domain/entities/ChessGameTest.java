2ccaea0a6d37776b46db97d43502e292af833339
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-03-25 20:14:35 +0400

    Test: additional for revert capture

--- src/test/java/core/project/chess/domain/entities/ChessGameTest.java
+++ src/test/java/core/project/chess/domain/entities/ChessGameTest.java
@@ -1056,6 +1056,10 @@ public class ChessGameTest {
 
         game.returnMovement(whitePlayer);
         game.returnMovement(blackPlayer);
+        System.out.println(navigator.prettyToString());
+
+        game.makeMovement(blackPlayer, d8, h4, new Queen(Color.BLACK));
+        System.out.println(navigator.prettyToString());
     }
 
     static Supplier<ChessGame> defaultChessGameSupplier() {
