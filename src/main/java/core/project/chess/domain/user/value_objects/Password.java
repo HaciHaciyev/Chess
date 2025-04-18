6b1f4b58e54438dbe34cebd0903b2828ba45c1ff
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-04-18 21:14:38 +0400

    Better messages for invalid situations

--- src/main/java/core/project/chess/domain/user/value_objects/Password.java
+++ src/main/java/core/project/chess/domain/user/value_objects/Password.java
@@ -5,20 +5,20 @@ import java.util.Objects;
 public record Password(String password) {
     public static final int MIN_SIZE = 8;
     public static final int MAX_SIZE = 64;
-    public static final String INVALID_PASSWORD = "Invalid password.";
+    public static final String INVALID_PASSWORD_SIZE = "Invalid password size: min - %s, max - %s";
 
     public static void validate(String password) {
         if (Objects.isNull(password)) {
-            throw new IllegalArgumentException(INVALID_PASSWORD);
+            throw new IllegalArgumentException("Password is null");
         }
         if (password.isBlank()) {
-            throw new IllegalArgumentException(INVALID_PASSWORD);
+            throw new IllegalArgumentException("Password is blank");
         }
         if (password.length() < MIN_SIZE) {
-            throw new IllegalArgumentException(INVALID_PASSWORD);
+            throw new IllegalArgumentException(INVALID_PASSWORD_SIZE.formatted(MIN_SIZE, MAX_SIZE));
         }
         if (password.length() > MAX_SIZE) {
-            throw new IllegalArgumentException(INVALID_PASSWORD);
+            throw new IllegalArgumentException(INVALID_PASSWORD_SIZE.formatted(MIN_SIZE, MAX_SIZE));
         }
     }
 }
