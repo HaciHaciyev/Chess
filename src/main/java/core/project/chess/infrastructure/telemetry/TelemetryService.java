4fc67aaa6730ca54ea3f3a741eb4a8bbbaf5b825
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-05-04 18:07:53 +0400

    better telemetry service

--- src/main/java/core/project/chess/infrastructure/telemetry/TelemetryService.java
+++ src/main/java/core/project/chess/infrastructure/telemetry/TelemetryService.java
@@ -1,11 +1,14 @@
 package core.project.chess.infrastructure.telemetry;
 
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
 @ApplicationScoped
 public class TelemetryService {
@@ -23,6 +26,8 @@ public class TelemetryService {
             action.run();
         } catch (Exception e) {
             span.recordException(e);
+            span.setStatus(StatusCode.ERROR, e.getMessage());
+            throw e;
         } finally {
             span.end();
         }
@@ -36,8 +41,107 @@ public class TelemetryService {
             action.run();
         } catch (Exception e) {
             span.recordException(e);
+            span.setStatus(StatusCode.ERROR, e.getMessage());
+            throw e;
         } finally {
             span.end();
         }
+    }
+
+    public void startWithChildSpan(String spanName, Runnable action) {
+        Span parentSpan = Span.current();
+        Span childSpan = tracer.spanBuilder(spanName)
+                .setParent(Context.current().with(parentSpan))
+                .startSpan();
+
+        try (Scope scope = childSpan.makeCurrent()) {
+            action.run();
+        } catch (Exception e) {
+            childSpan.recordException(e);
+            childSpan.setStatus(StatusCode.ERROR, e.getMessage());
+            throw e;
+        } finally {
+            childSpan.end();
+        }
+    }
+
+    public void startWithChildSpan(String spanName, Map<String, String> attributes, Runnable action) {
+        Span parentSpan = Span.current();
+        Span childSpan = tracer.spanBuilder(spanName)
+                .setParent(Context.current().with(parentSpan))
+                .startSpan();
+
+        attributes.forEach(childSpan::setAttribute);
+
+        try (Scope scope = childSpan.makeCurrent()) {
+            action.run();
+        } catch (Exception e) {
+            childSpan.recordException(e);
+            childSpan.setStatus(StatusCode.ERROR, e.getMessage());
+            throw e;
+        } finally {
+            childSpan.end();
+        }
+    }
+
+    public <T> T startWithChildSpan(String spanName, Supplier<T> action) {
+        Span parentSpan = Span.current();
+        Span childSpan = tracer.spanBuilder(spanName)
+                .setParent(Context.current().with(parentSpan))
+                .startSpan();
+
+        try (Scope scope = childSpan.makeCurrent()) {
+            return action.get();
+        } catch (Exception e) {
+            childSpan.recordException(e);
+            childSpan.setStatus(StatusCode.ERROR, e.getMessage());
+            throw e;
+        } finally {
+            childSpan.end();
+        }
+    }
+
+    public <T> T startWithChildSpan(String spanName, Map<String, String> attributes, Supplier<T> action) {
+        Span parentSpan = Span.current();
+        Span childSpan = tracer.spanBuilder(spanName)
+                .setParent(Context.current().with(parentSpan))
+                .startSpan();
+
+        attributes.forEach(childSpan::setAttribute);
+
+        try (Scope scope = childSpan.makeCurrent()) {
+            return action.get();
+        } catch (Exception e) {
+            childSpan.recordException(e);
+            childSpan.setStatus(StatusCode.ERROR, e.getMessage());
+            throw e;
+        } finally {
+            childSpan.end();
+        }
+    }
+
+    public Span getCurrentSpan() {
+        return Span.current();
+    }
+
+    public void addEvent(String eventName) {
+        Span.current().addEvent(eventName);
+    }
+
+    public void addEvent(String eventName, Map<String, String> attributes) {
+        Span span = Span.current().addEvent(eventName);
+        attributes.forEach(span::setAttribute);
+    }
+
+    public void setSpanAttribute(String key, String value) {
+        Span.current().setAttribute(key, value);
+    }
+
+    public void setSpanAttribute(String key, long value) {
+        Span.current().setAttribute(key, value);
+    }
+
+    public void setSpanAttribute(String key, boolean value) {
+        Span.current().setAttribute(key, value);
     }
 }
