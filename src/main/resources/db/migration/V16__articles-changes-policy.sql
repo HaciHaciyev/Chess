062ea3e5b912642b6c8b3b3059e9dc661a84188f
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-05-15 00:35:23 +0400

    Fix sql migrations
    Allow to manipulate with tags in non-published articles

--- src/main/resources/db/migration/V16__articles-changes-policy.sql
+++ src/main/resources/db/migration/V16__articles-changes-policy.sql
@@ -22,11 +22,6 @@ CREATE TRIGGER enforce_like_status
     FOR EACH ROW
     EXECUTE FUNCTION enforce_article_published();
 
-CREATE TRIGGER enforce_tag_status
-    BEFORE INSERT OR UPDATE OR DELETE ON ArticleTags
-    FOR EACH ROW
-    EXECUTE FUNCTION enforce_article_published();
-
 CREATE FUNCTION enforce_article_published_on_comment_likes() RETURNS TRIGGER AS $$
 BEGIN
     IF (SELECT a.status FROM Article a JOIN Comments c ON c.id = NEW.comment_id) <> 'PUBLISHED' THEN
